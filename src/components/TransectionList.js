9d036be44c2d8fdbac3332b4903aafe1a6a3178d
nandhini <nandhinimano11@gmail.com>
2020-12-20 15:33:46 +0530

    TransectionList

--- src/components/TransectionList.js
+++ src/components/TransectionList.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react'
 import { ListGroup } from 'react-bootstrap';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 class TransectionList extends Component {
-    render() {
-        return (
-          
-            <ListGroup >
-              {this.props.expenditure.length === 0 ? <p>No Spended money</p> :
-                this.props.expenditure.map((spend, index) => {
-                  return <ListGroup.Item style={{
-                    "display": "flex",
-                    "justifyContent": "space-between"
-                  }} key={index} variant={spend.spended ? "danger" : "success"}>
-                    <div>{spend.date}</div>
-                    <div>{spend.description}</div>
-                    <div>{"Rs."}{spend.moneyValue}</div>
-                  </ListGroup.Item>
-                })}
-            </ListGroup>
-        )
-    }
+  render() {
+    return (
+
+      <Row style={{ 'marginTop': "20px" }}>
+        <Col style={{ "backgroundColor": "white" }}>
+          <ListGroup >
+            {this.props.expenditure.length === 0 ? <p>No Spended money</p> :
+              this.props.expenditure.map((spend, index) => {
+                return <ListGroup.Item style={{
+                  "display": "flex",
+                  "justifyContent": "space-between"
+                }} key={index} variant={spend.spended ? "danger" : "success"}>
+                  <div>{spend.date}</div>
+                  <div>{spend.description}</div>
+                  <div>{"Rs."}{spend.moneyValue}</div>
+                </ListGroup.Item>
+              })}
+          </ListGroup></Col></Row>
+    )
+  }
 }
 
 TransectionList.propTypes = {
